523514bbd8fa752078d3cb0048eae03b3a27b0a6
NikhilSiddhu <nikhildsiddhu@outlook.com>
2025-11-10 05:28:01 +0530

    done for today

---
+++ calculator.py
@@ -0,0 +1,9 @@
+x = input("What's x?")
+
+y = input("What's y?")
+
+""" z = x + y not this but """
+print (int(x) + int(y))
+
+or 
+1:13:00
--- default.py
+++ default.py
@@ -1,3 +1,22 @@
-# Created on iPad.
+# Ask user for the name
+""" name = input("What's your name? ")
 
-print ('Hello World!')
+# Remove whitspace from str
+name = name.strip()
+
+# Capitalize user's first letter
+name = "" name.Capitalize() or for surname also "" name.title() """
+
+# Say Hello to user
+#print("Hello, ", "\"", name,"\"", sep=''), or# the given below 
+
+""" No Comma b/w f and " in print."""
+# Summarise all above in one line
+
+
+name = input("What's your name?").strip().title()
+
+#Split user name into fitst name and last name
+first, last = name.split(" ")
+
+print(f"Hello, {first}")
