8a67d84aaf93c8ca9b3bbadc74746c3053e474fb
NikhilSiddhu <nikhildsiddhu@outlook.com>
2025-11-11 22:16:14 +0530

    done for tday (But less)

--- calculator.py
+++ calculator.py
@@ -1,9 +1,24 @@
-x = input("What's x?")
+# Integer
+x = int(input("What's x?"))
+
+y = int(input("What's y?"))
+
+print (x + y)
+#or 
+
+""" x = input("What's x?")
 
 y = input("What's y?")
 
-""" z = x + y not this but """
+"" z = x + y not this but ""
 print (int(x) + int(y))
+or """
 
-or 
-1:13:00
+# NEXT
+
+# Float
+# Can use it to show and sum point values
+
+a = float(input("What is your no.1 ?"))
+b = float(input("What is your no.2 ?"))
+print(a+b)
